8e1ab2444f143c0786ea275c2e6b99362248b5b6
Tobias Bocanegra <tripod@adobe.com>
2022-09-13 14:20:13 +0200

    fix: update dependencies

--- src/version-to-git-tag.spec.js
+++ src/version-to-git-tag.spec.js
@@ -2,11 +2,10 @@ const versionToGitTag = require('./version-to-git-tag');
 
 describe('#versionToGitTag', () => {
   describe('if passed a falsy version', () => {
-    it('returns null rather than creating a bad git-tag', async done => {
+    it('returns null rather than creating a bad git-tag', async () => {
       expect(await versionToGitTag('')).toBe(null);
       expect(await versionToGitTag(undefined)).toBe(null);
       expect(await versionToGitTag(null)).toBe(null);
-      done();
     });
   });
 });
