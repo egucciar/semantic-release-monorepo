26ed3b2fd7c26b62533cc7b26246b2fa9acbb0cf
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2023-11-01 00:26:38 -0500

    ci: Remove npm plugin

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,24 @@
 const config = {
   extends: './index.js',
   branches: ['master'],
+  plugins: [
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd:
+          "yarn version ${nextRelease.version} && echo '::set-output name=version::${nextRelease.version}'",
+        publishCmd: 'yarn npm publish --access public',
+      },
+    ],
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        message:
+          'RELEASE: chore(release) - ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
 };
 export default config;
